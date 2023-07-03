c899a4a310ac89c6bb19d301f6c66591b9e5cc1a
Krossmanzs <radi.adi78@gmail.com>
2023-07-03 22:48:59 +0700

    First Init

---
+++ halo.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+// untuk mempersingkat perintah std
+using namespace std;
+
+int main() {
+    // penggunaan cout ketika tidak menggunakan using...
+    // std::cout << "halo ges" << std::endl;
+
+    // penggunaan cout ketika menggunakan using...
+    cout << "Halo ges" << endl;
+    cout << "nama saya linux" << endl;
+    cout << "saya mahasiswa IF" << endl;
+    return 0;
+}
